a446cdeed3a3531a519771dfa8752f03d2bdda73
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2019-04-04 00:08:29 +0200

    Add filter to pre_dispatch

--- simple-rest-cache.php
+++ simple-rest-cache.php
@@ -22,7 +22,7 @@ class SimpleRestCache
   private $server;
   const CACHE_DIR = __DIR__ . '/cache';
 
-  function __construct($result)
+  function __construct($request)
   {
     $this->server = rest_get_server();
 
@@ -30,10 +30,11 @@ class SimpleRestCache
       mkdir(self::CACHE_DIR);
     }
 
-    add_filter('rest_post_dispatch', [ $this, 'passThroughCache' ]);
+    // Filter any REST requests
+    add_filter('rest_pre_dispatch', [ $this, 'passThroughCache' ]);
   }
 
-  function passThroughCache($result)
+  function passThroughCache($request)
   {
     $requestUri = $_SERVER['REQUEST_URI'];
 
@@ -45,19 +46,22 @@ class SimpleRestCache
     $cacheFile = self::CACHE_DIR . '/' . $restEndpoint;
 
     if (file_exists($cacheFile)) {
-      // Found cache, get the response from cache
-      $response = new \WP_REST_Response();
-      $response->set_data(json_decode(file_get_contents($cacheFile), true));
-      return $response;
+      // Found cache, get the result from cache
+      $result = new \WP_REST_Response();
+      $result->set_data(json_decode(file_get_contents($cacheFile), true));
+      return $result;
 
     } else {
-      // Cache the result and return
+      // Manually dispatch the request, cache it and return results
+      remove_filter('rest_pre_dispatch', [ $this, 'passThroughCache' ]);
+      $result = rest_do_request(new \WP_REST_Request('GET', '/wp/v2/' . $restEndpoint));
       file_put_contents($cacheFile, json_encode($result->get_data()));
+      add_filter('rest_pre_dispatch', [ $this, 'passThroughCache' ]);
       return $result;
     }
   }
 }
 
-add_action('rest_api_init', function($result) {
-  return new SimpleRestCache($result);
+add_action('rest_api_init', function($request) {
+  return new SimpleRestCache($request);
 }, 4);
